ee4ee6e313aa116da14d8f38748e531da42ce56c
tmapana <tlotlisomapana@gmail.com>
2021-06-19 12:32:13 +0200

    Modified php script, removed error lines

--- contact-form.php
+++ contact-form.php
@@ -29,29 +29,7 @@ if(isset($_POST['email'])) {
     $email_from = $_POST['email']; // required
     $phone = $_POST['phone']; // required
     $message = $_POST['message']; // required
-     
-    $error_message = "";
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
- 
-  if(!preg_match($email_exp,$email_from)) {
-    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
-  }
- 
-    $string_exp = "/^[A-Za-z .'-]+$/";
- 
-  if(!preg_match($string_exp,$name)) {
-    $error_message .= 'The Name you entered does not appear to be valid.<br />';
-  }
- 
-   
-  if(strlen($message) < 1) {
-    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
-  }
- 
-  if(strlen($error_message) > 0) {
-    died($error_message);
-  }
- 
+    
     $email_message = "Form details below.\n\n";
  
      
