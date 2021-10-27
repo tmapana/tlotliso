8f1bf4c9b961ac673f3796458bbf9ebd888d784a
tmapana <tlotlisomapana@gmail.com>
2021-10-27 12:23:51 +0200

    Image gallery updated. Basic template to build on. Next work: about and contact.html

--- js/main.js
+++ js/main.js
@@ -1,65 +1,31 @@
-var fields = {};
+var slideIndex = 1;
+showSlides(slideIndex);
 
-document.addEventListener("DOMContentLoaded", function() {
-  fields.names = document.getElementById('names');
-  fields.email = document.getElementById('email');
-  fields.phone = document.getElementById('phone');
-  fields.message = document.getElementById('message');
-})
-
-function isNotEmpty(value) {
-  if (value == null || typeof value == 'undefined' ) return false;
-  return (value.length > 0);
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function isNumber(num) {
-  return (num.length > 0) && !isNaN(num);
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
 
-function isEmail(email) {
-  let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-  return regex.test(String(email).toLowerCase());
-}
-
-function fieldValidation(field, validationFunction) {
-  if (field == null) return false;
-
-  let isFieldValid = validationFunction(field.value)
-  if (!isFieldValid) {
-  field.className = 'placeholderRed';
-  } else {
-  field.className = '';
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
   }
-
-  return isFieldValid;
-}
-
-function isValid() {
-  var valid = true;
-
-  valid &= fieldValidation(fields.names, isNotEmpty);
-  valid &= fieldValidation(fields.email, isEmail);
-  valid &= fieldValidation(fields.phone, isNumber);
-  valid &= fieldValidation(fields.message, isNotEmpty);
-
-  return valid;
-}
-
-class User {
-  constructor(names, email, phone, message) {
-    this.names = names;
-    this.email = email;
-    this.phone = phone;
-    this.message = message;
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
   }
-}
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} 
 
-function contactMe() {
-  if (isValid()) {
-    let usr = new User(names.value, email.value, phone.value, message.value);
-
-    alert(`${usr.names} thanks for getting in touch. we will get back to you shortly.`)
-  } else {
-    alert("there was an error")
-  }
-}
+var slideIndex = 0;
+showSlides();
